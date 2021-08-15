868c691461ef6d2a62e11c0cec23b8d09dd422fb
SrishtiToora <77147148+SrishtiToora@users.noreply.github.com>
2021-08-15 16:23:26 +0530

    Final Keeper App

---
+++ build/precache-manifest.c82d8e96936f79613488612860c2b044.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa0a9e574c29bc51fc44bc7959f976b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7f30ba515d4e6407363",
+    "url": "/static/js/2.82dff26f.chunk.js"
+  },
+  {
+    "revision": "0dec55db6314b657f4ee",
+    "url": "/static/js/main.20bacc73.chunk.js"
+  },
+  {
+    "revision": "641f15582db5e9b7b4fe",
+    "url": "/static/js/runtime-main.2f3ca742.js"
+  }
+]);
